f2a7a5211e6f48b49d700e33966d7115bdc9ff26
guillaumemichel <guillaume.michel@epfl.ch>
2020-03-14 18:48:54 +0100

    cleaned code

---
+++ implementation/get_flights_information.py
@@ -0,0 +1,69 @@
+from traffic.data import aircraft, opensky, airports
+from traffic.core import Traffic
+from utils import *
+from flights import *
+import pandas as pd
+import math
+import requests
+
+import json
+
+# update aircraft db
+#aircraft.download_opensky()
+
+month = 'december01-30'
+start = "2019-12-01"
+end = "2019-12-31"
+output_path = '../data/'+month+'/'
+
+
+if __name__ == "__main__":
+    count = 0
+
+    usAirports = getUSAirports()
+
+    flights = loadFlights(start, end)
+    icaos = set()
+    for f in flights:
+        icaos.add(f.icao24)
+    total = len(icaos)
+    print("Found "+str(total)+" distinct icaos")
+
+    record = open(output_path+'record.txt', 'r')
+    done = record.read()
+    record.close()
+
+    for icao in icaos:
+        count+=1
+        if icao in done:
+            print(icao+' is already done')
+            continue
+
+        try:
+            aicrafts = opensky.api_aircraft(icao24=icao, begin=start+' 00:00', end="2019-12-30"+' 23:59')
+        except requests.exceptions.HTTPError as err:
+            if err.response.status_code == 404:
+                print("No result for icao "+icao)
+                continue
+            else:
+                raise
+
+        myFlights = Flights()
+        for _, row in aicrafts.iterrows():
+            flight = None
+            for f in flights:
+                # GET CLOSEST FLIGHT TO TIME WITH SAME CALLSIGN AND ICAO
+                if f.icao24 == row['icao24'] and (row['callsign'] in f.callsign or f.callsign in row['callsign'] or (f.registration is not None and f.registration in row['callsign'])) and (flight is None or abs(row['firstSeen']-f.start)<abs(row['firstSeen']-flight.start)):
+                    flight = f
+            if flight is None:
+                print("Flight not found!")
+                print(row)
+            else:
+                dep, arr = getFlightAirports(usAirports, flight, row)
+                myFlights.append(MyFlight(row['callsign'], row['icao24'], flight.registration, dep, arr))
+
+        myFlights.to_file(output_path+icao+'.json')
+        record = open(output_path+'record.txt', 'a')
+        record.write(icao+'\n')
+        record.close()
+        print('Done '+str(count)+'/'+str(total))
---
+++ implementation/utils.py
@@ -0,0 +1,101 @@
+from flights import *
+import math
+from traffic.core import Traffic
+from traffic.data import airports
+
+time_delta = 600 # min
+
+recordings_folder = '../data/recordings/'
+airport_file = '../data/usAirports.json'
+country = 'United States'
+
+def getAirportPosition(airports, icao):
+    a = airports[icao]
+    if a is None:
+        return None
+    lat, lon = a.latlon
+    alt = a.altitude
+    return Position(altitude=alt, latitude=lat, longitude=lon)
+
+def getAicraftPosition(flight, prop):
+    """
+    prop is 0 or 1, for departure and arrival position
+    """
+    step = 0.001
+    inc = (prop == 0)
+    while math.isnan(flight.at_ratio(prop).latitude) and ((inc and prop<1) or ((not inc) and prop>0)):
+        if inc:
+            prop += step
+        else:
+            prop -= step
+
+    if (inc and prop==1) or ((not inc) and prop==0):
+        return None
+
+    f = flight.at_ratio(prop)
+    lat, lon = f.latlon
+    alt = f.altitude
+
+    return Position(alt, lat, lon)
+
+def getFlightAirports(airportsList, flight, row):
+
+    if row['estDepartureAirport'] is None:
+        dep_ap_pos = None
+    else:
+        dep_ap_pos = getAirportPosition(airportsList, row['estDepartureAirport'])
+    dep_ac_pos = getAicraftPosition(flight, 0)
+    if dep_ac_pos != None:
+        dep = TakeoffLanding(airport=row['estDepartureAirport'], time=row['firstSeen'], airport_position=dep_ap_pos, aircraft_position=dep_ac_pos)
+    else:
+        dep = TakeoffLanding(airport=row['estDepartureAirport'], time=row['firstSeen'], airport_position=dep_ap_pos, aircraft_position=None)
+
+    if row['estArrivalAirport'] is None:
+        arr_ap_pos = None
+    else:
+        arr_ap_pos = getAirportPosition(airportsList, row['estArrivalAirport'])
+    arr_ac_pos = getAicraftPosition(flight, 1)
+    if arr_ac_pos != None:
+        arr = TakeoffLanding(airport=row['estArrivalAirport'], time=row['lastSeen'], airport_position=arr_ap_pos, aircraft_position=arr_ac_pos)
+    else:
+        arr = TakeoffLanding(airport=row['estArrivalAirport'], time=row['lastSeen'], airport_position=arr_ap_pos, aircraft_position=None)
+
+    return dep, arr
+
+def getUSAirports():
+    usAirports = airports.search(country)
+    usAirports.to_json(airport_file)
+    return usAirports
+
+def parseAirportsICAO():
+    f = open(airport_file, 'r')
+    txt = f.read()
+    f.close()
+
+    txt = txt[txt.find('icao'):]
+    txt = txt[txt.find('{'):]
+
+    icaos = list()
+
+    while True:
+    #for i in range(10):
+        start = txt.find(':"')
+        if txt.find('}') < start:
+            break
+        txt = txt[start+2:]
+        end = txt.find('"')
+        icaos.append(txt[:end])
+        txt = txt[end:]
+
+    return icaos    
+
+def getFlightsFilename(start_day, end_day):
+    return start_day + "_" + end_day + "_DCM_recordings.parquet"
+
+def loadFlights(start_day, end_day):
+    filename = recordings_folder+getFlightsFilename(start_day, end_day)
+    flights = Traffic.from_file(filename)
+
+    print("Loaded "+str(len(flights))+" flights")
+
+    return flights
